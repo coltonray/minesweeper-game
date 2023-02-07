09291c102f4f4a446eac31cda31d98082d34c39f
coltonray <coltonray501@gmail.com>
2023-02-07 05:11:19 -0600

    added generateMines
    grid now populates mines randomly.

--- app.js
+++ app.js
@@ -1,21 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.getElementById("grid");
     const board = [];
+    
+    const rows = 10;
+    const columns = 10;
+    const mines = 12;
+    const mineLocations = generateMines();
 
     generateGrid(); 
 
+    //return an array of mineLocations
+    function generateMines() {
+        let useableMines = mines;
+        const m = [];
+        
+        while (useableMines > 0) {
+            let r = Math.floor(Math.random() * rows);
+            let c = Math.floor(Math.random()*columns);
+
+            let id = r.toString() + "" + c.toString();
+
+            if(!m.includes(id)) {
+                m.push(id)
+                useableMines -= 1;
+            }
+        }
+        return m;
+    }
+    
+    //create and populate grid
     function generateGrid() {
-        for (var i = 0; i < 10; i++){
+        for (var i = 0; i < rows; i++){
             let row = [];
-            for(var j = 0; j < 10; j++){
+            for(var j = 0; j < columns; j++){
                 let cell = document.createElement('div');
-                cell.setAttribute('id', i +""+ j);
+                cell.setAttribute('id', i + "" + j);
                 grid.appendChild(cell);
                 row.push(cell);
+                if(mineLocations.includes(cell.getAttribute('id'))){
+                    cell.classList.add('mine');
+                }
+                else {
+                    cell.classList.add('clear');
+                }
             }
             board.push(row);
         }
     }
+
+    
 
 
 
